5f3aa324f73662057a1c18c99c6424413a106a78
Rajat Bhatt <r.bhatt@easternenterprise.com>
2019-05-12 22:08:56 +0530

    Shape layer fix

--- GraphAnimation/ViewController.swift
+++ GraphAnimation/ViewController.swift
@@ -20,7 +20,11 @@ class ViewController: UIViewController {
     var startPosition: CGPoint! //Start position for the gesture transition
     var originalHeight: CGFloat = 0 // Initial Height for the UIView
     var difference: CGFloat!
-    let shapeLayer = CAShapeLayer()
+    enum PanDirection: Int {
+        case up
+        case down
+    }
+    var currentDirection = PanDirection.up
     
     // MARK: Navigation controller life cycle methods
     // View did load
@@ -41,7 +45,7 @@ class ViewController: UIViewController {
         addPanGestureOnView()
         self.originalHeight = self.animationView.frame.height
         self.originalFrame = CGRect(x: self.animationView.frame.origin.x, y: self.animationView.frame.origin.y, width: self.animationView.frame.width, height: self.animationView.frame.height)
-        print("Original frame: \(originalFrame)")
+        self.animationView.backgroundColor = #colorLiteral(red: 0.2615871429, green: 0.8389324546, blue: 0.4879657626, alpha: 1)
     }
     
     func addPanGestureOnView() {
@@ -49,101 +53,145 @@ class ViewController: UIViewController {
         self.animationView.addGestureRecognizer(panGesture)
     }
     
-    func halfPoint1D(p0: CGFloat, p2: CGFloat, control: CGFloat) -> CGFloat {
-        return 2 * control - p0 / 2 - p2 / 2
-    }
-    
     @objc func panGestureAction(_ panGestureRecognizer: UIPanGestureRecognizer) {
         let point = panGestureRecognizer.location(in: self.view)
+        let direction: PanDirection = (panGestureRecognizer.velocity(in: self.view).y > 0) ? .down : .up
+        self.currentDirection = direction
         translation = panGestureRecognizer.translation(in: self.view)
-        let velocity = (-panGestureRecognizer.velocity(in: self.view).y)/(-translation.y)
-        let window = UIApplication.shared.keyWindow
-        let bottomPadding = window?.safeAreaInsets.bottom
-        let areaRemoved = (bottomPadding ?? 0) + 55
-        let maxHeight = self.backgroundImageView.frame.height - areaRemoved
+//        let velocity = (-panGestureRecognizer.velocity(in: self.view).y)/(-translation.y)
         if panGestureRecognizer.state == .began {
             startPosition = panGestureRecognizer.location(in: self.animationView) // the postion at which PanGestue Started
         }
         
         if panGestureRecognizer.state == .began || panGestureRecognizer.state == .changed {
-            panGestureRecognizer.setTranslation(CGPoint(x: 0.0, y: 0.0), in: self.view)
-            let endPosition = panGestureRecognizer.location(in: animationView) // the posiion at which PanGesture Ended
-            difference = endPosition.y - startPosition.y
-            var newFrame = animationView.frame
-            if (newFrame.height >= (originalHeight)) && (newFrame.height <= maxHeight) {
-                newFrame.origin.x = animationView.frame.origin.x
-                newFrame.origin.y = animationView.frame.origin.y + difference //Gesture Moving Upward will produce a negative value for difference
-                newFrame.size.width = animationView.frame.size.width
-                newFrame.size.height = animationView.frame.size.height - difference //Gesture Moving Upward will produce a negative value for difference
-                
-//                let path = UIBezierPath()
-//                path.move(to: newFrame.origin)
-//                let y:CGFloat = newFrame.origin.y
-//                path.addQuadCurve(to: CGPoint(x: animationView.frame.size.width, y: y), controlPoint: CGPoint(x: point.x, y: newFrame.origin.y - (newFrame.height)))
-//                path.addLine(to: CGPoint(x: newFrame.maxX, y: newFrame.maxY))
-//                path.addLine(to: CGPoint(x: newFrame.minX, y: newFrame.maxY))
-//                path.close()
-//                shapeLayer.path = path.cgPath
-                self.animationView.layer.addSublayer(shapeLayer)
-                UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: velocity, options: .curveEaseInOut, animations: {
-                    self.animationView.frame = newFrame
-                }, completion: nil)
-            }
+            panBegan(panGestureRecognizer: panGestureRecognizer, direction: direction, point: point)
         }
         
         if panGestureRecognizer.state == .ended || panGestureRecognizer.state == .cancelled {
-            if originalHeight > self.animationView.frame.height {
-                let heightDifference = originalHeight - self.animationView.frame.height
-                var newFrame = animationView.frame
-                newFrame.origin.x = animationView.frame.origin.x
-                newFrame.origin.y = animationView.frame.origin.y - heightDifference //Gesture Moving Upward will produce a negative value for difference
-                newFrame.size.width = animationView.frame.size.width
-                newFrame.size.height = animationView.frame.size.height + heightDifference
-                UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
-                    self.animationView.frame = newFrame
-                }, completion: nil)
-            } else if self.animationView.frame.height > maxHeight {
-                let heightDifference = self.animationView.frame.height - maxHeight
-                var newFrame = animationView.frame
-                newFrame.origin.x = animationView.frame.origin.x
-                newFrame.origin.y = animationView.frame.origin.y + heightDifference //Gesture Moving Upward will produce a negative value for difference
-                newFrame.size.width = animationView.frame.size.width
-                newFrame.size.height = animationView.frame.size.height - heightDifference
-                UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
-                    self.animationView.frame = newFrame
-                }, completion: nil)
+            panEnded()
+        }
+        panGestureRecognizer.reset()
+    }
+    
+    func getMaxAnimatedViewHeight() -> CGFloat {
+        let window = UIApplication.shared.keyWindow
+        let bottomPadding = window?.safeAreaInsets.bottom
+        let areaRemoved = (bottomPadding ?? 0) + 55
+        return self.backgroundImageView.frame.height - areaRemoved
+    }
+    
+    func panBegan(panGestureRecognizer: UIPanGestureRecognizer, direction: PanDirection, point: CGPoint) {
+        panGestureRecognizer.setTranslation(CGPoint(x: 0.0, y: 0.0), in: self.view)
+        let maxHeight = getMaxAnimatedViewHeight()
+        let endPosition = panGestureRecognizer.location(in: animationView) // the posiion at which PanGesture Ended
+        difference = endPosition.y - startPosition.y
+        var newFrame = animationView.frame
+        if (newFrame.height >= (originalHeight)) && (newFrame.height <= maxHeight) {
+            newFrame.origin.x = animationView.frame.origin.x
+            newFrame.origin.y = animationView.frame.origin.y + difference //Gesture Moving Upward will produce a negative value for difference
+            newFrame.size.width = animationView.frame.size.width
+            newFrame.size.height = animationView.frame.size.height - difference //Gesture Moving Upward will produce a negative value for difference
+            self.animationView.frame = newFrame
+            switch direction {
+            case .up:
+                self.getTopCurve(newFrame: newFrame, point: point)
+            case.down:
+                self.getBottomCurve(newFrame: newFrame, point: point)
             }
         }
-        panGestureRecognizer.setTranslation(CGPoint.zero, in: self.animationView)
     }
-}
-
-extension UIPanGestureRecognizer {
     
-    public struct PanGestureDirection: OptionSet {
-        public let rawValue: UInt8
+    func panEnded() {
+//        animatePath()
+        self.resetView()
+        let maxHeight = getMaxAnimatedViewHeight()
+        var newFrame = animationView.frame
+        newFrame.origin.x = animationView.frame.origin.x
+        newFrame.size.width = animationView.frame.size.width
         
-        public init(rawValue: UInt8) {
-            self.rawValue = rawValue
+        if originalHeight > self.animationView.frame.height {
+            let heightDifference = originalHeight - self.animationView.frame.height
+            newFrame.origin.y = animationView.frame.origin.y - heightDifference
+            newFrame.size.height = animationView.frame.size.height + heightDifference
+            self.animationView.frame = newFrame
+            //                    UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: velocity, options: .curveEaseInOut, animations: {
+            //
+            //                    }, completion: nil)
+        } else if self.animationView.frame.height > maxHeight {
+            let heightDifference = self.animationView.frame.height - maxHeight
+            
+            newFrame.origin.y = animationView.frame.origin.y + heightDifference
+            newFrame.size.height = animationView.frame.size.height - heightDifference
+            self.animationView.frame = newFrame
+            //                    UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: velocity, options: .curveEaseInOut, animations: {
+            //
+            //                    }, completion: nil)
         }
-        
-        static let Up = PanGestureDirection(rawValue: 1 << 0)
-        static let Down = PanGestureDirection(rawValue: 1 << 1)
-        static let Left = PanGestureDirection(rawValue: 1 << 2)
-        static let Right = PanGestureDirection(rawValue: 1 << 3)
     }
     
-    private func getDirectionBy(velocity: CGFloat, greater: PanGestureDirection, lower: PanGestureDirection) -> PanGestureDirection {
-        if velocity == 0 {
-            return []
-        }
-        return velocity > 0 ? greater : lower
+    func getTopCurve(newFrame: CGRect, point: CGPoint) {
+        self.resetView()
+        self.animationView.layer.masksToBounds = false
+        self.animationView.clipsToBounds = false
+        self.animationView.layer.mask = nil
+        let origin = CGPoint(x: 0.0, y: 0.0)
+        let shapeLayer = CAShapeLayer()
+        let path = UIBezierPath()
+        path.move(to: origin)
+        path.addQuadCurve(to: CGPoint(x: animationView.frame.size.width, y: origin.y), controlPoint: CGPoint(x: point.x, y: origin.y - 50))
+        path.addLine(to: CGPoint(x: newFrame.width, y: newFrame.height))
+        path.addLine(to: CGPoint(x: 0, y: newFrame.height))
+        path.close()
+        shapeLayer.path = path.cgPath
+        shapeLayer.fillColor = #colorLiteral(red: 0.2615871429, green: 0.8389324546, blue: 0.4879657626, alpha: 1).cgColor
+        self.animationView.layer.addSublayer(shapeLayer)
     }
     
-    public func direction(in view: UIView) -> PanGestureDirection {
-        let velocity = self.velocity(in: view)
-        let yDirection = getDirectionBy(velocity: velocity.y, greater: PanGestureDirection.Down, lower: PanGestureDirection.Up)
-        let xDirection = getDirectionBy(velocity: velocity.x, greater: PanGestureDirection.Right, lower: PanGestureDirection.Left)
-        return xDirection.union(yDirection)
+    func getBottomCurve(newFrame: CGRect, point: CGPoint) {
+        self.resetView()
+        self.animationView.clipsToBounds = true
+        let origin = CGPoint(x: 0.0, y: 0.0)
+        let shapeLayer = CAShapeLayer()
+        let path = UIBezierPath()
+        path.move(to: origin)
+        path.addQuadCurve(to: CGPoint(x: animationView.frame.size.width, y: origin.y), controlPoint: CGPoint(x: point.x, y: origin.y + 50))
+        path.addLine(to: CGPoint(x: newFrame.width, y: newFrame.height))
+        path.addLine(to: CGPoint(x: 0, y: newFrame.height))
+        path.close()
+        shapeLayer.path = path.cgPath
+//        animatePath()
+        self.animationView.layer.addSublayer(shapeLayer)
+        self.animationView.layer.mask = shapeLayer
+        self.animationView.layer.masksToBounds = true
     }
+    
+    func resetView() {
+        if currentDirection == .up {
+            if let subLayers = self.animationView.layer.sublayers {
+                for layer in subLayers {
+                    layer.removeFromSuperlayer()
+                }
+            }
+        }
+        self.animationView.layer.mask = nil
+    }
+    
+//    func animatePath() {
+//        let shapeLayer = CAShapeLayer()
+//        let origin = CGPoint(x: 0.0, y: 0.0)
+//        let path = UIBezierPath()
+//        path.move(to: origin)
+//        path.addLine(to: CGPoint(x: self.animationView.frame.width, y: 0))
+//        path.addLine(to: CGPoint(x: self.animationView.frame.width, y: self.animationView.frame.height))
+//        path.addLine(to: CGPoint(x: 0, y: self.animationView.frame.height))
+//        path.close()
+//        let pathAnimation = CABasicAnimation(keyPath: "path")
+//        pathAnimation.toValue = path.cgPath
+//        pathAnimation.duration = 0.75
+//        pathAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+//        pathAnimation.autoreverses = false
+//        pathAnimation.repeatCount = .leastNonzeroMagnitude
+//        shapeLayer.add(pathAnimation, forKey: "pathAnimation")
+//        shapeLayer.path = path.cgPath
+//    }
 }
